992f740752c204e88ee8698a69e5d7a474e350f2
Haolun <chshawkn@hotmail.com>
2017-04-29 00:38:48 +0800

    fix ci script

--- ci.sh
+++ ci.sh
@@ -5,6 +5,7 @@ if [ -f codesigning.asc.enc ] && [ "${TRAVIS_PULL_REQUEST}" == 'false' ]; then
     gpg --fast-import codesigning.asc
 fi
 
+
 ### OSS CI CONTEXT VARIABLES BEGIN
 if ([ -z "${CI_BUILD_REF_NAME}" ] && [ -n "${TRAVIS_BRANCH}" ]); then CI_BUILD_REF_NAME="${TRAVIS_BRANCH}"; fi
 if [ -n "${OSS_BUILD_REF_BRANCH}" ]; then BUILD_SCRIPT_REF="${OSS_BUILD_REF_BRANCH}"; else BUILD_SCRIPT_REF="develop"; fi
@@ -47,15 +48,15 @@ if ([ "${GIT_REPO_OWNER}" == "${BUILD_HOME1_OSS_OWNER}" ] && [ "pull_request" !=
         release*)
             export BUILD_PUBLISH_CHANNEL="release";
             if [ "${1}" == "publish_snapshot" ]; then
-                publish_release ;
+                publish_release
             elif [ "${1}" == "analysis" ]; then
                 echo "skip analysis as not at develop branch";
             else
-                $@;
+                $@
             fi
             ;;
         feature*|hotfix*|"master"|*)
-            if [ "${1}" == "test_and_build" ]; then $@ ; fi
+            if [ "${1}" == "test_and_build" ]; then $@; fi
             echo "on this condition only trigger test_and_build,CI_BUILD_REF_NAME=${CI_BUILD_REF_NAME}"
             ;;
     esac

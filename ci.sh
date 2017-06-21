c4c940a0f190a9f221f1042afa26300d21564686
Haolun <chshawkn@hotmail.com>
2017-06-21 13:51:45 +0800

    ci.sh format

--- ci.sh
+++ ci.sh
@@ -16,6 +16,6 @@ export BUILD_SITE_PATH_PREFIX="oss"
 #if [ -z "${LIB_CI_SCRIPT}" ]; then LIB_CI_SCRIPT="https://github.com/home1-oss/oss-build/raw/develop/src/main/ci-script/lib_ci.sh"; fi
 if [ -z "${LIB_CI_SCRIPT}" ]; then LIB_CI_SCRIPT="http://gitlab.local:10080/home1-oss/oss-build/raw/develop/src/main/ci-script/lib_ci.sh"; fi
 echo "eval \$(curl -s -L ${LIB_CI_SCRIPT})"
-source src/main/ci-script/lib_ci.sh
 #eval "$(curl -s -L ${LIB_CI_SCRIPT})"
+source src/main/ci-script/lib_ci.sh
 ### OSS CI CALL REMOTE CI SCRIPT END

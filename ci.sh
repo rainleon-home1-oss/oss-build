781c63f22ffd9eb06536a0b014a6d54456cb01a7
Haolun <chshawkn@hotmail.com>
2017-06-29 17:13:05 +0800

    fix ci config

--- ci.sh
+++ ci.sh
@@ -3,6 +3,7 @@
 if [ -f codesigning.asc.enc ] && [ "${TRAVIS_PULL_REQUEST}" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_f1fe46eea14b_key -iv $encrypted_f1fe46eea14b_iv -in codesigning.asc.enc -out codesigning.asc -d
     gpg --fast-import codesigning.asc
+    if [ -n "${GPG_KEYID}" ]; then gpg --keyring secring.gpg --export-secret-key ${GPG_KEYID} > secring.gpg; fi
 fi
 
 export BUILD_SITE_PATH_PREFIX="oss"
